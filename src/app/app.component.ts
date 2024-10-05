6c5c948feb186369e1220d63864b0a303d9af790
latifaweb <emailuntukkomputerkantor@gmail.com>
2024-10-05 16:17:47 +0700

    ketiga

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  phoneNumber: string = '085729272228'; // Nomor yang akan disalin
+  phoneNumber: string = '0857 2927 2228'; // Nomor yang akan disalin
   copied: boolean = false; // Status tombol copy
+  showPopup: boolean = false; // Status tulisan popup
 
   // Fungsi untuk menyalin teks ke clipboard
   copyToClipboard() {
     navigator.clipboard.writeText(this.phoneNumber).then(
       () => {
-        this.copied = true; // Ubah status tombol menjadi Copied
+        this.copied = true; // Ubah status tombol menjadi "No Whatsapp Tersalin"
+        this.showPopup = true; // Tampilkan tulisan popup
+
         setTimeout(() => {
-          this.copied = false; // Kembalikan ke "Copy" setelah 2 detik
-        }, 2000);
+          this.copied = false; // Kembalikan ke "Salin No Whatsapp" setelah 2 detik
+          this.showPopup = false; // Sembunyikan popup setelah 4 detik
+        }, 5000); // Sembunyikan popup setelah 4 detik
       },
       (err) => {
         console.error('Gagal menyalin teks: ', err);
